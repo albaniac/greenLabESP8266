79e2989caee8096a7f5a2dd2d6c2d72f5bbae182
Vas2016 <vasily.yuryev.a1@gmail.com>
2018-02-28 11:55:12 +0700

    arduino

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include <ESP8266mDNS.h>
 #include "FS.h"
 #include <ESP8266HTTPUpdateServer.h>
+#include <Wire.h>
+// #include "U8glib.h"
 
 #include <ArduinoJson.h>
 #include "StringCommand.h"
@@ -30,6 +32,40 @@ WifiEsp wifie = WifiEsp();
 // WifiEsp wifi = new WifiEsp();
 ESP8266WebServer server(80);
 FSBrowser fsb;
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE); // I2C / TWI
+// void draw(void)
+// {
+//   // graphic commands to redraw the complete screen should be placed here
+//   u8g.setFont(u8g_font_unifont);
+//   //u8g.setFont(u8g_font_osb21);
+//   u8g.drawStr(0, 22, "Hello World!");
+// }
+
+// void setup(void)
+// {
+  // flip screen, if required
+  // u8g.setRot180();
+
+  // set SPI backup if required
+  //u8g.setHardwareBackup(u8g_backup_avr_spi);
+
+  // assign default color value
+
+  // pinMode(8, OUTPUT);
+// }
+
+// void loop(void)
+// {
+//   // picture loop
+//   u8g.firstPage();
+//   do
+//   {
+//     draw();
+//   } while (u8g.nextPage());
+
+//   // rebuild the picture after some delay
+//   //delay(50);
+// }
 
 void updateWifi()
 {
@@ -80,6 +116,24 @@ void setup()
     }
     Serial.printf("\n");
   }
+  // Wire.pins(5, 4);
+
+  // if (u8g.getMode() == U8G_MODE_R3G3B2)
+  // {
+  //   u8g.setColorIndex(255); // white
+  // }
+  // else if (u8g.getMode() == U8G_MODE_GRAY2BIT)
+  // {
+  //   u8g.setColorIndex(3); // max intensity
+  // }
+  // else if (u8g.getMode() == U8G_MODE_BW)
+  // {
+  //   u8g.setColorIndex(1); // pixel on
+  // }
+  // else if (u8g.getMode() == U8G_MODE_HICOLOR)
+  // {
+  //   u8g.setHiColorByRGB(255, 255, 255);
+  // }
 
   loadConfig(config);
 
@@ -94,5 +148,7 @@ void setup()
 }
 void loop()
 {
+  // u8g.firstPage();
+  // draw();
   server.handleClient();
 }
